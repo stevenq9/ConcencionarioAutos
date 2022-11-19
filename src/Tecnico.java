35cc1e8e50fb8472be9479d8c670e6271f3381de
Steven Quispe <steven.quispe@epn.edu.ec>
2022-11-19 18:06:08 -0500

    Refactoring 4: Move Method "RegistrarRespaldo" on "GestorPersonal" to "Tecnico"

--- src/Tecnico.java
+++ src/Tecnico.java
@@ -1,3 +1,8 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+
 public class Tecnico {
     private String nombreTecnico;
     private int numeroTecnico;
@@ -10,5 +15,30 @@ public class Tecnico {
     @Override
     public String toString() {
         return "\n Nombre: " + nombreTecnico + "\n Numero: " + numeroTecnico;
+    }
+
+
+    //Registrar Respaldo: Crear archivo de nuevo tecnico registrado
+    public void RegistrarRespaldo(){  //Registro tecnico en el archivo de respaldo
+        FileWriter w;
+        BufferedWriter bw;
+        PrintWriter wr;
+        try{
+            //Crear respaldo de nuevo Tecnico Registrado
+            File respaldo = new File("C:\\Users\\Steven\\Desktop\\PersonalConcesionario\\" +
+                    this.nombreTecnico.toUpperCase() +".txt");
+            w = new FileWriter(respaldo);
+            bw = new BufferedWriter(w);
+            wr = new PrintWriter(bw);
+            wr.append(toString());
+            wr.close();
+            bw.close();
+            //Informacion Respaldo Creado
+            System.out.println("--------------------- Tecnico Resgistrado ------------------------------------");
+            System.out.println(toString());
+            System.out.println("---------------------------------------------------------");
+        }catch(Exception e){
+            System.out.println("Error al registrar Respaldo");
+        }
     }
 }
